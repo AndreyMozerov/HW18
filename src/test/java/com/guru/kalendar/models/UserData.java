945e33c18e2771febbbc9e36adbb513c83ebf562
Andrey Mozerov <andrey-mozerov@mail.ru>
2023-02-19 00:06:33 +0300

    v2

---
+++ src/test/java/com/guru/kalendar/models/UserData.java
@@ -0,0 +1,19 @@
+package com.guru.kalendar.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public class UserData {
+        private User data;
+
+        public User getData() {
+            return data;
+        }
+
+        public void setData(User data) {
+            this.data = data;
+        }
+
+
+    }
